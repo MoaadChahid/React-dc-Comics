b359911cda3164c5bd88a4be368fc298b31daff1
MoaadChahid <chahidmoaad1@gmail.com>
2026-08-21 03:23:45 +0200

    fine repo, completamento main e footer, mancanza .active, e spostamento shop in main

--- Cd-Comics/src/components/Footer.jsx
+++ Cd-Comics/src/components/Footer.jsx
@@ -1,9 +1,55 @@
+import './Footer.css';
+
+import Facebook from '../assets/footer-facebook.png';
+import Periscope from '../assets/footer-periscope.png';
+import Pinterest from '../assets/footer-pinterest.png';
+import Twitter from '../assets/footer-twitter.png';
+import Youtube from '../assets/footer-youtube.png';
+
+
+
+
+
+const Bottone = () => (
+
+
+    <div className='BarraFooter'>
+        <button className="btn">
+            SIGN-UP NOW!
+        </button>
+
+
+        <section className='titleFooter'>
+
+            <h2 >FOLLOW US</h2>
+            <section className='smallImage'>
+                <img className='FooterMargin' src={Facebook} alt="Facebook" />
+                <img className='FooterMargin' src={Periscope} alt="Periscope" />
+                <img className='FooterMargin' src={Pinterest} alt="Pinterest" />
+                <img className='FooterMargin' src={Twitter} alt="Twitter" />
+                <img className='FooterMargin' src={Youtube} alt="Youtube" />
+
+
+
+            </section>
+
+
+
+        </section>
+
+    </div>
+)
+
+
+
+
+
 export const Footer = () => (
 
     <>
 
 
-
+        <Bottone />
 
 
     </>
--- Cd-Comics/src/components/Main.jsx
+++ Cd-Comics/src/components/Main.jsx
@@ -5,6 +5,9 @@ import merchandiseComics from '../assets/buy-comics-merchandise.png';
 import shopLocator from '../assets/buy-comics-shop-locator.png';
 import subscriptionsComics from '../assets/buy-comics-subscriptions.png';
 import dcPowerVisa from '../assets/buy-dc-power-visa.svg';
+import LogoBackGround from '../assets/dc-logo-bg.png';
+import FooterBg from '../assets/footer-bg.jpg';
+
 
 const Content = () => (
     <a href="" className='link'>-- Content Goes Here --</a>
@@ -14,24 +17,100 @@ const Descrizione = () => (
 
     <div className='Background_image'>
 
-        <img className='img_one' src={digitalComics} alt="DigitalComics" />
+        <img className='img' src={digitalComics} alt="DigitalComics" />
         <p className='nomeone'>DIGITAL COMICS</p>
 
-        <img className='img_one' src={merchandiseComics} alt="DigitalComics" />
+        <img className='img' src={merchandiseComics} alt="DigitalComics" />
         <p className='nomeone'> DC MERCHANDISE</p>
 
-        <img className='img_one' src={subscriptionsComics} alt="DigitalComics" />
+        <img className='img' src={subscriptionsComics} alt="DigitalComics" />
         <p className='nomeone'> SUBSCRIPTIONS</p>
 
-        <img className='img_one' src={shopLocator} alt="DigitalComics" />
+        <img className='img' src={shopLocator} alt="DigitalComics" />
         <p className='nomeone'> COMICS SHOP LOCAL</p>
 
-        <img className='img_one' src={dcPowerVisa} alt="DigitalComics" />
+        <img className='img' src={dcPowerVisa} alt="DigitalComics" />
         <p className='nomeone'> DC POWER VISA</p>
 
 
     </div>
 )
+
+const Lista = () => (
+
+    <div className='Contenitori'>
+        <ul className='List'>
+            <h1 className='title' >DC COMICS</h1>
+            <section className='Caratteri'>
+                <p>Charectrs</p>
+                <p>Comics</p>
+                <p>Movies</p>
+                <p>TV</p>
+                <p>Games</p>
+                <p>Videos</p>
+                <p>News</p>
+
+            </section>
+
+
+
+        </ul>
+
+        <ul className='List'>
+            <h1 className='title' >SHOP</h1>
+            <section className='Caratteri'>
+                <p>Shop DC</p>
+                <p>Shop DC Collectibles</p>
+
+            </section>
+
+        </ul>
+
+        <ul className='List'>
+            <h1 className='title' >DC </h1>
+            <section className='Caratteri'>
+                <p>Terms Of Use</p>
+                <p>Privacy policy (New)</p>
+                <p>Ad Choices</p>
+                <p>Advertising</p>
+                <p>Jobs</p>
+                <p>Subscriptions</p>
+                <p>Talent Workshops</p>
+                <p>CPSC Certificates</p>
+                <p>Ratings</p>
+                <p>Shop Help</p>
+                <p>Contact Us</p>
+
+            </section>
+
+        </ul>
+        <ul className='List'>
+            <h1 className='title' >SITES </h1>
+            <section className='Caratteri'>
+                <p>DC</p>
+                <p>MAD Magazine</p>
+                <p>DC Kids</p>
+                <p>DC Universe</p>
+                <p>DC Power Visa</p>
+
+            </section>
+
+
+
+        </ul>
+
+
+        <section >
+
+            <img className="Bgimage" src={LogoBackGround} alt="BgDescrizione" />
+
+        </section>
+
+    </div>
+
+
+
+);
 
 
 export const Main = () => (
@@ -41,6 +120,8 @@ export const Main = () => (
 
         <Content />
         <Descrizione />
+        <Lista />
+
 
 
     </main>
